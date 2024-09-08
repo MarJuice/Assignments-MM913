e59b816bda5a9686c6b049711725c43ddbba96e3
marjuice <mariushanss1@gmail.com>
2024-09-08 19:39:13 +0200

    Task finished

--- practice2.js
+++ practice2.js
@@ -27,8 +27,9 @@ for(let index = 0; index < people.length; index++){
     Husk vi bruker console.log() til å skrive ting ut.
 */
 console.log("Oppgave: A");
-
-
+for(let index = 0; index < 100; index++){
+    console.log('Debugging er som å være en detektiv i et krim drama hvor du også er morderen');
+}
 
 /*
     Oppgave: B
@@ -39,26 +40,24 @@ console.log("Oppgave: A");
 console.log("Oppgave: B");
 
 const max = 99;
-for (let index = 0; index < max; index--); {
-  console.log(index + " flasker med brus i kassa");
-  console.log(index + " flasker med brus i kassa");
-
-  console.log(
-    "Ta en ut, drikk den opp." + (index - 1) + " flasker med brus i kassa"
-  );
+for(let index = 0; index < max; index++) {
+  console.log(max - index + " flasker med brus i kassa");
+  console.log("Ta en ut, drikk den opp. " + (max - index - 1) + " flasker med brus i kassa");
   console.log("");
 }
-
 console.log("Ingen flere flasker med brus i kassa");
-
 
 /*
     Oppgave: C
     Deklarere en variabel for tyngdekraft og en variabel for pi og en variabel for antall personer i et rom. 
 */
 console.log("Oppgave: C");
-
-
+let gravity = 9.81; // Gravity is different on other planets, so unsure if it should be a const
+const pi = Math.PI;
+let numOfPeopleInRoom = 2;
+console.log(`The force of gravity is ${gravity} m/s^2.`);
+console.log(pi);
+console.log(`There are currently ${numOfPeopleInRoom} people in the room.`);
 
 /*
     Oppgave: D
@@ -67,8 +66,9 @@ console.log("Oppgave: C");
 console.log("Oppgave: D");
 
 // lag funksjonen din her. 
-
-
+function add(num1,num2){
+    return num1+num2;
+}
 console.log("3 + 6 = " + add(3,6));
 console.log("5673 + 234 = " + add(5673,234));
 
@@ -77,25 +77,38 @@ console.log("5673 + 234 = " + add(5673,234));
     Bruk variabelen people fra eksempelet og skriv navna ut i reversert rekke følge (bruk en løkke)
     NB du skal ikke endre på people, bare bruke det som den inneholder.
 */
-console.log("Oppgave: D");
 
+console.log("Oppgave: E");
+for(index=people.length-1; index>=0; index--){
+    let person = people[index];
+    console.log(person);
+}
 
 
 /*
     Oppgave: F
     Deklarere en variabel for en liste med telefon nummer. Listen din skal ha minst 3 nummer. 
 */
-console.log("Oppgave: D");
 
+let phoneNumbers = ['12345678', '87654321', '11223344'];
 
 /*
     Oppgave: G
     Skriv ut bare det siste telefon numeret i listen din. 
 */
 
+console.log("Oppgave: G");
+let lastPhoneNumber = phoneNumbers[phoneNumbers.length-1];
+console.log(lastPhoneNumber);
+
 /*
     Oppgave: H
     Skriv ut kunn etternavnene for personene i listen, bruk en løkke.
 */
 
+console.log("Oppgave: H");
 let personer = [["Christian", "Simonsen"], ["Tony", "Bergholtz"]]
+for(index = 0; index < personer.length; index++){
+    let lastNames = personer[index][personer.length-1];
+    console.log(lastNames);
+}
