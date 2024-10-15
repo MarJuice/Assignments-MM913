29aee37ed0b0955715537b1617cca9597285b399
marjuice <mariushanss1@gmail.com>
2024-10-15 13:55:38 +0200

    Added english dictionary

--- module1/steinSaksPapir/rps.mjs
+++ module1/steinSaksPapir/rps.mjs
@@ -8,8 +8,10 @@ import { ANSI } from './lib/ansi.mjs';
 import { getRandomItemFromArray } from './lib/random.mjs';
 import GAME_DICTIONARY from './dictionary.mjs';
 
+
 const CHOICES = { rock: 1, paper: 2, scissors: 3 };
 const LIST_OF_CHOICES = [CHOICES.rock, CHOICES.paper, CHOICES.scissors];
+
 
 print(ANSI.CLEAR_SCREEN);
 print(GAME_DICTIONARY.title, ANSI.COLOR.RED);
