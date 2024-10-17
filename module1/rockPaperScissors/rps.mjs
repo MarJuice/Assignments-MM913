8d9bc331770136dd7db6b69b2f31eb378ab00c78
marjuice <mariushanss1@gmail.com>
2024-10-17 22:37:58 +0200

    Added start menu, replay and exit option after each round, language support and ANSI colors, ASCII art in the console, changed evaluateWinner() to support ASCII for player 2 winning

---
+++ module1/rockPaperScissors/rps.mjs
@@ -0,0 +1,132 @@
+//#region 
+import * as readlinePromises from 'node:readline/promises';
+const rl = readlinePromises.createInterface({ input: process.stdin, output: process.stdout });
+//#endregion
+
+import { print } from './lib/output.mjs';
+import { ANSI } from './lib/ansi.mjs';
+import { getRandomItemFromArray } from './lib/random.mjs';
+import { GAME_DICTIONARY, ART } from './dictionary.mjs';
+
+
+const CHOICES = { rock: 1, paper: 2, scissors: 3 };
+const LIST_OF_CHOICES = [CHOICES.rock, CHOICES.paper, CHOICES.scissors];
+let language = null;
+
+print(ANSI.CLEAR_SCREEN);
+
+async function startMenu() {
+
+    while (language === null) {
+        console.clear();
+        print(GAME_DICTIONARY.global.prompt);
+        let selectedLanguage = await rl.question("");
+
+        if (selectedLanguage.toLowerCase() === "no") {
+            language = {
+                ...GAME_DICTIONARY.no,
+                ...ART.no
+            };
+        } else if (selectedLanguage.toLowerCase() === "en") {
+            language = { 
+                ...GAME_DICTIONARY.en,
+                ...ART.en
+            };
+        } 
+    }
+
+    print(language.continue);
+    await rl.question("");
+
+    print(language.startScreen, ANSI.COLOR.YELLOW);
+    startGame();
+}
+
+async function startGame() {
+    print(language.title);
+
+    let player = await askForPlayerChoice();
+    let npc = makeAIChoice();
+    
+    print(`${language.youPicked} ${getDesc(player)} ${language.aiPicked} ${getDesc(npc)}`);
+    print(language.winner + evaluateWinner(player, npc));
+    
+    // ---- Game functions etc..
+    
+    function evaluateWinner(p1Ch, p2Ch) {
+        let result = language.player2;
+    
+        if (p1Ch == p2Ch) {
+            result = language.draw;
+            print(language.drawScreen, ANSI.COLOR.YELLOW);
+        }
+        else if (p1Ch === CHOICES.rock && p2Ch === CHOICES.scissors) {
+            result = language.player1;
+            print(language.winScreen, ANSI.COLOR.GREEN);
+        } else if (p1Ch === CHOICES.paper && p2Ch === CHOICES.rock) {
+            result = language.player1;
+            print(language.winScreen, ANSI.COLOR.GREEN); 
+        } else if (p1Ch === CHOICES.scissors && p2Ch === CHOICES.paper) {
+            result = language.player1;
+            print(language.winScreen, ANSI.COLOR.GREEN);
+        } else { 
+            print(language.loseScreen, ANSI.COLOR.RED);
+        }
+        return result;
+    }
+    
+    function makeAIChoice() {
+        return getRandomItemFromArray(LIST_OF_CHOICES);
+    }
+    
+    function getDesc(choice) {
+        return language.choices[choice - 1]
+    }
+    
+    async function askForPlayerChoice() {
+    
+        let choice = null;
+    
+        do {
+            print(language.selectionQuestion);
+            let rawChoice = await rl.question("");
+            rawChoice = rawChoice.toUpperCase();
+            choice = evaluatePlayerChoice(rawChoice);
+        } while (choice == null)
+    
+        return choice;
+    }
+    
+    function evaluatePlayerChoice(rawChoice) {
+        let choice = null;
+    
+        if (rawChoice == language.rock) {
+            choice = CHOICES.rock;
+        } else if (rawChoice == language.paper) {
+            choice = CHOICES.paper;
+        } else if (rawChoice == language.scissors) {
+            choice = CHOICES.scissors;
+        }
+        return choice;
+    }
+
+    askToRestart()
+}
+
+async function askToRestart() {
+    print(language.restart);
+
+    let choice = await rl.question("");
+
+    if (choice.toUpperCase() === "X") {
+        print(language.exit, ANSI.COLOR.RED);
+        process.exit();
+    } else if (choice === "") {
+        print("...\n")
+        startGame();
+    } else {
+        askToRestart();
+    }
+}
+
+startMenu();
