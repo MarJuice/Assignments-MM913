8f78b4437b5c31b565bb345e03d51f625dc3f791
Marius <79099572+MarJuice@users.noreply.github.com>
2024-11-07 13:05:20 +0100

    Add files via upload

---
+++ module2/hangman/dictionary.mjs
@@ -0,0 +1,15 @@
+const dictionary = {
+    no:{
+        wrongGuesses: "Feil gjetninger",
+        guessPrompt: "Gjett en bokstav eller ord",
+        winCelebration: "Hurra du gjettet ordet"
+    },
+    en:{
+        wrongGuesses: "Wrong guesses",
+        guessPrompt: "Guess a letter or word",
+        winCelebration: "Congratulations you guessed the word"
+    }
+ 
+}
+
+export default dictionary
