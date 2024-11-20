0be02fe30376c15d5b9e80b83429a0927b83616a
marjuice <mariushanss1@gmail.com>
2024-11-20 21:44:21 +0100

    Added title screen, no longer allows duplicate answers, last frame of graphics array now displays properly (somewhat), player now has more tries until the man is hanged, statistics such as total guesses and time played are displayed after each round, random words are picked from words.txt or ord.txt depending on the language chosen

--- module2/hangman/game.mjs
+++ module2/hangman/game.mjs
@@ -3,19 +3,20 @@ import fs from "node:fs"
 const rl = readlinePromises.createInterface({ input: process.stdin, output: process.stdout });
 
 import { HANGMAN_UI } from './graphics.mjs';
-import { GREEN, RED, WHITE, RESET, CYAN } from './colors.mjs';
+import { GREEN, RED, WHITE, RESET, CYAN, YELLOW } from './colors.mjs';
 import dictionary from './dictionary.mjs';
 import { splash, titleScreen } from './splash.mjs';
+import { start } from 'node:repl';
 
-let word = getRandomWord();
-let guessedWord = createGuessList(word.length);
+let wordList = [];
+let word = null;
+let guessedWord = [];
 let wrongGuesses = [];
 let language = null;
 let isGameOver = false;
+let timePlayed = 0;
 
-do { 
-    await languageSelection(); 
-} while (language == null) 
+await languageSelection(); 
 
 async function languageSelection() {
     let selectedLanguage = await rl.question(splash);
@@ -24,53 +25,129 @@ async function languageSelection() {
         language = dictionary.no;
     } else if (selectedLanguage.toLowerCase() == "en") {
         language = dictionary.en;
-    } 
+    } else {
+        language = null;
+        console.clear();
+        await languageSelection();
+    }
+    
+    wordList = getWordsFromFile();
+    
+    await startMenu();
+    
+}
+
+function getWordsFromFile() {
+    let fileName = '';
+
+    if (language == dictionary.en) {
+        fileName = 'words.txt';
+    } else if (language == dictionary.no) {
+        fileName = 'ord.txt';
+    }
+
+
+    const data = fs.readFileSync(`./lib/${fileName}`, 'utf8');
+    const words = data.split('\n');
+
+    for (let i = 0; i < words.length; i++) {
+        words[i] = words[i].trim();
+    }
+
+    return words;
+}
+
+async function startMenu() {
+    
+    do {
+        console.clear();
+        print(titleScreen, RED);
+        let menuChoice = await rl.question(language.menuOptions);
+        if (menuChoice == 1) {
+            await game();
+        } else if (menuChoice == 2) {
+            language = null;
+            await languageSelection();
+        } else if (menuChoice == 3) {
+            print(language.exitMessage);
+            process.exit();
+        } else {
+            menuChoice = null;
+            await startMenu();
+        }
+    } while (menuChoice = null);
+} 
+
+function resetGameState() {
+    word = getRandomWord();
+    guessedWord = createGuessList(word.length);
+    wrongGuesses = [];
+    isGameOver = false;
+    timePlayed = 0;
 }
 
 async function game() {
+
+    resetGameState();
+
+    let guessedLetters = [];
+    let startTime = Date.now();
+
     do {
         updateUI();
 
-        let guess = (await rl.question(language.guessPrompt)).toLowerCase();
+        let guess = (await rl.question(`${language.guessPrompt}: `)).toLowerCase();
 
+
+        if (guessedLetters.includes(guess)) {
+            continue;
+        }
+
+        guessedLetters.push(guess);
+        
         if (isWordGuessed(word, guess)) {
-            print(language.winCelebration, GREEN);
+            print(`${language.winCelebration}: '${word}'`, GREEN);
             isGameOver = true;
         }
-        else if (word.includes(guess)) {
+        
+        if (word.includes(guess)) {
 
-            uppdateGuessedWord(guess);
+            updateGuessedWord(guess);
 
             if (isWordGuessed(word, guessedWord)) {
-                print(language.correctGuess, GREEN);
+                print(`${language.winCelebration}: '${word}'`, GREEN);
                 isGameOver = true;
             }
         } else {
-            print(language.wrongGuess, RED);
-            wrongGuesses.push(guess);
+            
+            wrongGuesses.push(guess);            
 
             if (wrongGuesses.length >= HANGMAN_UI.length - 1) {
+                updateUI();
+                print(`${language.deathMessage}. '${word}' ${language.correctWord}.`, RED);                
                 isGameOver = true;
-                print(language.deathMessage, RED);
             }
         }
 
     } while (isGameOver == false)
 
-    replay();
+    let endTime = Date.now();
+    timePlayed = (endTime - startTime) / 1000;
+    print(`${language.statisticsGuesses}: ${guessedLetters.length}`, YELLOW);
+    print(`${language.statisticsTime}: ${timePlayed}s`, YELLOW);
+
+    await replay();
 
     async function replay() {
         print(language.replayQuestion)
         let replayAnswer = (await rl.question(language.replayOptions)).toLowerCase();
         if (replayAnswer == ""){
-            word = getRandomWord();
-            guessedWord = createGuessList(word.length);
-            wrongGuesses = [];
-            isGameOver = false;
+            resetGameState();
             await game();
         } else if (replayAnswer == "x"){
-            print(language.exitMessage);
-            process.exit();        
+            resetGameState();
+            console.clear();
+            await startMenu();       
         } else {
             console.clear();
             await replay();
@@ -78,8 +155,7 @@ async function game() {
     }
 }
 
-
-function uppdateGuessedWord(guess) {
+function updateGuessedWord(guess) {
     for (let i = 0; i < word.length; i++) {
         if (word[i] == guess) {
             guessedWord[i] = guess;
@@ -90,10 +166,12 @@ function uppdateGuessedWord(guess) {
 }
 
 function createGuessList(length) {
-    let output = [];
+    let output = [];    
+
     for (let i = 0; i < length; i++) {
         output[i] = "_";
     }
+    
     return output;
 }
 
@@ -103,7 +181,6 @@ function isWordGuessed(correct, guess) {
             return false;
         }
     }
-
     return true;
 }
 
@@ -112,7 +189,6 @@ function print(msg, color = WHITE) {
 }
 
 function updateUI() {
-
     console.clear();
     print(guessedWord.join(""), GREEN);
     print(HANGMAN_UI[wrongGuesses.length]);
@@ -122,11 +198,6 @@ function updateUI() {
 }
 
 function getRandomWord() {
-
-    const words = ["Kiwi", "Car", "Dog", "etwas"];
-    let index = Math.floor(Math.random() * words.length);
-    return words[index].toLowerCase();
-
+    const randomIndex = Math.floor(Math.random() * wordList.length);
+    return wordList[randomIndex].toLowerCase();
 }
-
-game();
