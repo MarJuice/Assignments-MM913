3ec95b225302a80584a583ab28e25cb2a793ac91
Marius <79099572+MarJuice@users.noreply.github.com>
2024-11-28 10:59:04 +0100

    Fixed table

--- module3/ticTacToe/game.mjs
+++ module3/ticTacToe/game.mjs
@@ -120,9 +120,9 @@ console.log(`
     ╔═══╦═══╦═══╗
     ║ ${board[0]}  ║ ${board[1]} ║ ${board[2]} ║
     ╠═══╬═══╬═══╣
-    ║ ${board[0]} ║ ${board[0]} ║ ${board[0]} ║
+    ║ ${board[3]} ║ ${board[4]} ║ ${board[5]} ║
     ╠═══╬═══╬═══╣
-    ║ ${board[0]} ║ ${board[0]} ║ ${board[0]} ║
+    ║ ${board[6]} ║ ${board[7]} ║ ${board[8]} ║
     ╚═══╩═══╩═══╝
  `)
 }
