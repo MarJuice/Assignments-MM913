4025541fc906a1c6f8948fd39debec3d445c9bfa
marjuice <mariushanss1@gmail.com>
2024-12-12 12:03:31 +0100

    Added support for splash screen and start menu, executes the game afterwards

--- module4/labyrinth/labyrinth.mjs
+++ module4/labyrinth/labyrinth.mjs
@@ -2,14 +2,25 @@ import ANSI from "./ANSI.mjs";
 import { FPS } from "./gameConstants.mjs";
 import { update } from "./updateGame.mjs";
 import { draw } from "./drawGame.mjs";
+import { splashScreen, startMenu } from "./menus.mjs";
+import * as readlinePromises from 'node:readline/promises';
+const rl = readlinePromises.createInterface({ input: process.stdin, output: process.stdout });
 
-// Clear the screen and prepare the terminal for game rendering
-console.log(ANSI.CLEAR_SCREEN + ANSI.CURSOR_HOME + ANSI.HIDE_CURSOR);
-
-// Start the game loop using setInterval 
+// Game loop, update the game state then draw it
 function gameLoop() {
     update();
     draw();
 }
 
-setInterval(gameLoop, FPS);
+// Ensures the game doesn't start instantly, has to wait for the player to go through menu first
+async function startGame() {
+    await splashScreen();
+    await startMenu();
+    setInterval(gameLoop, FPS); // Start the game loop using setInterval 
+}
+
+// Clear the screen and prepare the terminal for game rendering
+console.log(ANSI.CLEAR_SCREEN + ANSI.CURSOR_HOME + ANSI.HIDE_CURSOR);
+startGame();
+
+export { startGame };
